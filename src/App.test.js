e0c711a3c9268b97ff32a9484a70f74666c986c8
devopsFabio <fpradoaraujo@hotmail.com>
2025-11-22 00:25:50 -0300

    Corrige pipeline.yml e App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Fabio Prado de Araújo title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Fabio Prado de Araújo/i);
+  expect(titleElement).toBeInTheDocument();
 });
